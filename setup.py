da233002157c2199b084c7a8e986fc429107a3d5
Khizhnyakov <khizhnyakov.av@obt-vlg.ru>
2020-02-04 15:09:56 +0400

    setup script set up settings.py and urls.py too

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup, Extension
+# -*- coding: utf-8 -*-
+from distutils.core import setup
 setup(
     name='m3_project.app',
     version='0.1.0',
-    packages=['app',],
-    #requiries=["django==1.11", "m3-objectpack==2.2.25"]
-    scripts=['__setup_script.py',]
+    packages=['app', ],
+    py_modules=['manage','m3_project/settings','m3_project/urls'],
+    requiries=["django==1.11", "m3-objectpack==2.2.25"],
 )
